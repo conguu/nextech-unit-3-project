8df5bf4b010549381a3a1677ebbccddabef8271c
congu <connerguillerman@gmail.com>
2026-06-17 23:30:36 -0500

    Finish all choices and add all assets

--- js/options.js
+++ js/options.js
@@ -1,68 +1,357 @@
 var options = {
-    levels: {
-        start: {
-            message: ["Your alarm clock rings, waking you up."],
-            choices: [
+	levels: {
+		start: {
+			message: ["Your alarm clock rings, waking you up."],
+			choices: [
+				{
+					option: "Get out of bed",
+					nextLevel: "bedroom",
+				},
+
+				{
+					option: "Go back to sleep",
+					nextLevel: "dream",
+				},
+			],
+		},
+
+		dream: {
+			message: ["You fall asleep, dreaming of...?"],
+			choices: [
+				{
+					option: "Falling off of a building",
+					nextLevel: "creed",
+				},
+
+				{
+					option: "67",
+					nextLevel: "sixseven",
+				},
+			],
+		},
+
+		creed: {
+			message: ["Hold me now"],
+			choices: [
+				{
+					option: "Wake up",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		sixseven: {
+			message: ["67"],
+			choices: [
+				{
+					option: "Wake up",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		bedroom: {
+			message: ["You get out of bed, alarm clock still ringing."],
+			choices: [
+				{
+					option: "Turn off alarm clock",
+					nextLevel: "nightStand",
+				},
+
+				{
+					option: "Leave your bedroom",
+					nextLevel: "hallNaked",
+				},
+			],
+		},
+
+		hallNaked: {
+			message: [
+				"You leave your bedroom, forgetting to put on clothes. <br> Your dad finds you naked.",
+			],
+			choices: [
+				{
+					option: "Restart",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		nightStand: {
+			message: [
+				"You walk over to your nightstand and turn off your alarm clock.",
+			],
+			choices: [
+				{
+					option: "Get your clothes on",
+					nextLevel: "closet",
+				},
+
+				{
+					option: "Go back to bed",
+					nextLevel: "dream",
+				},
+			],
+		},
+
+		closet: {
+			message: ["You open your closet. <br> What should you wear?"],
+			choices: [
+				{
+					image: "assets/fortnite.png",
+					nextLevel: "clothed",
+				},
+				{
+					image: "assets/shrek.jpeg",
+					nextLevel: "clothed",
+				},
+			],
+		},
+
+		clothed: {
+			message: ["You should probably leave your bedroom now."],
+			choices: [
+				{
+					option: "Walk out the door",
+					nextLevel: "hall",
+				},
+				{
+					option: "Jump out of the window",
+					nextLevel: "outside",
+				},
+			],
+		},
+
+		outside: {
+			message: [
+				"You jump out of the window and die. <br> You live on the third floor, what'd you expect?",
+			],
+			choices: [
+				{
+					option: "Restart",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		hall: {
+			message: ["You walk out the door to find a stranger standing outside."],
+			choices: [
+				{
+					option: "Say hi",
+					nextLevel: "hi",
+				},
+				{
+					option: "Challenge him",
+					nextLevel: "challenge",
+				},
+			],
+		},
+
+		hi: {
+			message: ["You say hi. <br> He was just going to the bathroom."],
+			choices: [
+				{
+					option: "Walk downstairs",
+					nextLevel: "stairs",
+				},
+			],
+		},
+
+		stairs: {
+			message: [
+				"You start walking downstairs. <br> Pick a number to determine the outcome.",
+			],
+			choices: [
+				{
+					option: "1",
+					nextLevel: "carCrash",
+				},
+				{
+					option: "2",
+					nextLevel: "banana",
+				},
+				{
+					option: "3",
+					nextLevel: "continue",
+				},
+			],
+		},
+
+		carCrash: {
+			message: [
+				"As you're walking down the stairs, a car suddenly crashes through the wall. <br> It destroyed every bone in your body.",
+			],
+			choices: [
+				{
+					option: "Restart",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		banana: {
+			message: [
+				"As you're walking down the stairs, you slip on a banana. <br> It causes you to trip and die.",
+			],
+			choices: [
+				{
+					option: "Restart",
+					nextLevel: "start",
+				},
+			],
+		},
+
+		continue: {
+			message: [
+				"You make it all the way down safely. <br> What should you do next?",
+			],
+			choices: [
+				{
+					option: "Find the dog",
+					nextLevel: "dogFind",
+				},
+				{
+					option: "Go outside",
+					nextLevel: "outside",
+				},
+			],
+		},
+
+		dogFind: {
+			message: [
+				"You decide to find the dog. <br> Where should you start your search?",
+			],
+			choices: [
+				{
+					option: "In the living room",
+					nextLevel: "dogLivingRoom",
+				},
+				{
+					option: "In the kitchen",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
+
+		dogLivingRoom: {
+			message: [
+				"You enter the living room to find the dog. <br> Where should you look?",
+			],
+			choices: [
+				{
+					option: "Behind the couch",
+					nextLevel: "dogCouch",
+				},
+				{
+					option: "Under a blanket",
+					nextLevel: "dogBlanket",
+				},
                 {
-                    option: "Get out of bed",
-                    nextLevel: "bedroom",
-                },
+					option: "Look in the kitchen",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
 
+		dogCouch: {
+			message: [
+				"You check behind the couch and find an omnious black cube. <br> It sucks you in and you die.",
+			],
+			choices: [
+				{
+					option: "Restart",
+					nextLevel: "start",
+				},
+			],
+		},
+
+        dogBlanket: {
+			message: [
+				"You found him! <br> There are many endings; try to get them all!",
+			],
+			choices: [
+				{
+					option: "Play again?",
+					nextLevel: "start",
+				},
+			],
+		},
+
+        dogKitchen: {
+			message: [
+				"You check the kitchen for the dog. <br> Where should you look?",
+			],
+			choices: [
+				{
+					option: "Under the table",
+					nextLevel: "dogTable",
+				},
+				{
+					option: "Inside of the dog's crate",
+					nextLevel: "dogCrate",
+				},
                 {
-                    option: "Go back to sleep",
-                    nextLevel: "dream",
-                },
-            ]
-        },
+					option: "Look in the living room",
+					nextLevel: "dogLivingRoom",
+				},
+			],
+		},
 
-        bedroom: {
-            message: ["You get out of bed, alarm clock still ringing."],
-            choices: [
+        dogTable: {
+			message: [
+				"You check under the table, but find nothing.",
+			],
+			choices: [
+				{
+					option: "Go back",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
+
+        dogCrate: {
+			message: [
+				"You check inside of the dogs crate, but find nothing.",
+			],
+			choices: [
+				{
+					option: "Go back",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
+
+        outside: {
+			message: [
+				"You walk outside and randomly spot a cat dancing.",
+			],
+			choices: [
                 {
-                    option: "Turn off alarm clock",
-                    nextLevel: "nightStand",
-                },
+					option: "Dance with it",
+					nextLevel: "dance",
+				},
+				{
+					option: "Go back",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
 
+        dance: {
+			message: [
+				"You dance with the cat.",
+			],
+			choices: [
                 {
-                    option: "Leave your bedroom",
-                    nextLevel: "hallNaked",
-                },
-            ]
-        },
-
-        dream: {
-            message: ["You go back to sleep, dreaming of..."],
-            choices: [
-                {
-                    option: "Falling off of a building",
-                    nextLevel: "creed",
-                },
-
-                {
-                    option: "67",
-                    nextLevel: "sixseven",
-                },
-            ]
-        },
-
-            creed: {
-                message: [""],
-                choices: [
-                    {
-                        option: "Wake up",
-                        nextLevel: "start",
-                    },
-                ]
-            },
-
-            sixseven: {
-                message: [""],
-                choices: [
-                    {
-                        option: "Wake up",
-                        nextLevel: "start",
-                    },
-                ]
-            },
-    }
-}
+					option: "Dance more",
+					nextLevel: "dance",
+				},
+				{
+					option: "Go back",
+					nextLevel: "dogKitchen",
+				},
+			],
+		},
+	},
+};
--- js/script.js
+++ js/script.js
@@ -1,74 +1,208 @@
 let container = document.querySelector("#container");
 let title = document.querySelector("#title");
 let choicesContainer = document.querySelector("#choices");
+let cat = document.querySelector("#cat");
+
+const oof = new Audio("assets/oof.mp3");
+const holdmenow = new Audio("assets/creed.mp3");
+const sixseven = new Audio("assets/67-sound.mp3");
+const ominousMusic = new Audio("assets/ominousBlackCube.mp3");
+const catMusic = new Audio("assets/silly-cat.mp3")
+const challengerApproaching = new Audio(
+	"assets/champ/challenger-approaching.mp3",
+);
+const battle = new Audio("assets/champ/battle.mp3");
 
 var past = [options.levels.start];
 var firstLevel = options.levels.start;
 renderLevel(firstLevel);
 
-choicesContainer.addEventListener("click", function(event) {
-    if (event.target.tagName === "BUTTON") {
-        let clickedButton = event.target;
-        var nextLevel = clickedButton.getAttribute("data-next-level");
-        
-        container.classList.add("fadeOut");
-        renderLevel(options.levels[nextLevel]);
-        
-        console.log("below is the normal thing passed in when choice is clicked");
-        console.log(options.levels[nextLevel]);
-        
-        container.classList.remove("fadeOut");
-        container.classList.add("fadeIn");
-        
-        past.push(options.levels[nextLevel]);
-        console.log(past);
-    }
+choicesContainer.addEventListener("click", function (event) {
+	if (event.target.tagName === "BUTTON") {
+		let clickedButton = event.target;
+		var nextLevel = clickedButton.getAttribute("data-next-level");
+
+		if (nextLevel === "challenge") {
+			triggerChallengeSequence();
+			return;
+		} 
+        if (nextLevel === "dogCouch") {
+		    triggerOminousSequence();
+            return;
+        }
+
+		container.classList.add("fadeOut");
+		renderLevel(options.levels[nextLevel], nextLevel);
+
+		console.log("below is the normal thing passed in when choice is clicked");
+		(console.log(options.levels[nextLevel]), nextLevel);
+
+		container.classList.remove("fadeOut");
+		container.classList.add("fadeIn");
+
+		past.push(options.levels[nextLevel]);
+		console.log(past);
+
+		if (nextLevel === "challenge") {
+			challenge();
+		}
+	}
 });
 
-function renderLevel(level) {
-    choicesContainer.innerHTML = "";
-    
-    if (!level.message || level.message.length === 0) {
-        callback();
-        return;
-    }
+function renderLevel(level, levelName) {
+	console.log(level);
+	choicesContainer.innerHTML = "";
 
-    title.innerHTML = level.message[0];
-    
-    if (level.message.length > 1) {
-        let i = 1;
-        
-        function time() {
-            setTimeout(function() {
-                title.innerHTML = level.message[i];
-                i++;
-                
-                if (i < level.message.length) { 
-                    time();
-                } else {
-                    callback(); 
-                }
-            }, 1500);
-        }
-        
-        time();
-    } else {callback();}
-    
-    function callback() {
-        choicesContainer.innerHTML = "";
+	if (levelName === "creed") {
+		document.body.style.backgroundImage = "url('assets/creed-one-last-breath.gif')";
+		holdmenow.play().catch(function (error) {
+			console.log("Audio playback waiting for user interaction:", error);
+		});
+	} else if (levelName === "sixseven") {
+		document.body.style.backgroundImage = "url('assets/67.gif')";
+		sixseven.play().catch(function (error) {
+			console.log("Audio playback waiting for user interaction:", error);
+		});
+	} else if (levelName === "outside" || levelName === "dance") {
+        cat.style.opacity = 1;
+        cat.style.left = "32.5%";
+        cat.style.top = "20%";
+        catMusic.play().catch(function (error) {
+			console.log("Audio playback waiting for user interaction:", error);
+		});
+    } else {
+		document.body.style.backdropFilter = "none";
+		document.body.style.backgroundImage = "none";
+		holdmenow.pause();
+		holdmenow.currentTime = 0;
+		sixseven.pause();
+		sixseven.currentTime = 0;
+		oof.pause();
+		oof.currentTime = 0;
+	}
 
-        var levelChoices = level.choices;
-        if (levelChoices) {
-            for (var i = 0; i < levelChoices.length; i++) {
-                var choiceData = levelChoices[i];
-                var nextLevelAttr = choiceData.nextLevel;
-                var buttonText = choiceData.option;
+	if (level.choices.some((choice) => choice.option === "Restart")) {
+		document.body.style.backdropFilter = "brightness(0.75)";
+		oof.play().catch(function (error) {
+			console.log("Audio playback waiting for user interaction:", error);
+		});
+	}
 
-                choicesContainer.insertAdjacentHTML(
-                    "beforeend", 
-                    "<button data-next-level='" + nextLevelAttr + "'>" + buttonText + "</button>"
-                );
-            }
-        }
-    }
+	if (!level.message || level.message.length === 0) {
+		callback();
+		return;
+	}
+
+	title.innerHTML = level.message[0];
+
+	if (level.message.length > 1) {
+		let i = 1;
+
+		function time() {
+			setTimeout(function () {
+				title.innerHTML = level.message[i];
+				i++;
+
+				if (i < level.message.length) {
+					time();
+				} else {
+					callback();
+				}
+			}, 1500);
+		}
+
+		time();
+	} else {
+		callback();
+	}
+
+	function callback() {
+		choicesContainer.innerHTML = "";
+
+		var levelChoices = level.choices;
+		if (levelChoices) {
+			for (var i = 0; i < levelChoices.length; i++) {
+				var choiceData = levelChoices[i];
+				var nextLevelAttr = choiceData.nextLevel;
+				var buttonText = choiceData.option;
+
+				if (choiceData.image) {
+					buttonContent =
+						"<img src='" + choiceData.image + "' alt='" + buttonText + "' />";
+				} else {
+					buttonContent = buttonText;
+				}
+
+				choicesContainer.insertAdjacentHTML(
+					"beforeend",
+					"<button data-next-level='" +
+						nextLevelAttr +
+						"'>" +
+						buttonContent +
+						"</button>",
+				);
+			}
+		}
+	}
+}
+
+function triggerOminousSequence() {
+	let ominousBlackCube = document.querySelector("#ominousBlackCube");
+
+	ominousMusic.play().catch(function (error) {
+		console.log("Audio playback waiting for user interaction:", error);
+	});
+    
+    oof.volume = 0;
+    container.classList.add("fadeOut");
+    document.body.style.backgroundColor = "black";
+
+    setTimeout(function() {
+        ominousBlackCube.style.left = "32.5%";
+        ominousBlackCube.style.opacity = "1";
+        setTimeout(function () {
+            ominousBlackCube.style.left = "-100%";
+            ominousBlackCube.style.opacity = "0";
+            ominousMusic.pause();
+            ominousMusic.currentTime = 0;
+            oof.volume = 1;
+            document.body.style.backdropFilter = "brightness(0.75)";
+            document.body.style.backgroundColor = "slategray";
+            container.classList.remove("fadeOut");
+            container.classList.add("fadeIn");
+            renderLevel(options.levels["dogCouch"], "dogCouch");
+        }, 15000);
+    }, 500);
+}
+
+function triggerChallengeSequence() {
+	let newFoeImg = document.querySelector("#newFoeImage");
+	let champ = document.querySelector("#champ");
+
+	challengerApproaching.play().catch(function (error) {
+		console.log("Audio playback waiting for user interaction:", error);
+	});
+    
+	container.classList.add("fadeOut");
+
+	newFoeImg.style.left = "0";
+	newFoeImg.style.opacity = "1";
+
+	setTimeout(function () {
+		newFoeImg.style.left = "100%";
+		newFoeImg.style.opacity = "0";
+
+		setTimeout(function () {
+			newFoeImg.style.left = "-50%";
+		}, 200);
+
+		setTimeout(function () {
+			battle.play().catch(function (error) {
+				console.log("Audio playback waiting for user interaction:", error);
+			});
+
+			champ.style.left = "40%";
+			champ.style.opacity = "1";
+		}, 500);
+	}, 2250);
 }
